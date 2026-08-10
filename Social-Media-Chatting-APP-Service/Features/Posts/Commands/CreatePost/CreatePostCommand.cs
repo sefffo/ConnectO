8c561c33089feb098a819cc9b3368af6661c333c
pc <pc@DESKTOP-2N68CP9>
2026-08-10 17:25:19 +0300

    feat(posts): add create post command and handler
    - Introduced `CreatePostCommand` and corresponding handler for post creation functionality.
    - Added support for handling post types (e.g., Repost, Quote) and associating media assets with posts.
    - Updated project structure to include `Features\Posts\Commands`.

---
+++ Social-Media-Chatting-APP-Service/Features/Posts/Commands/CreatePost/CreatePostCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.PostsDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Posts.Commands.CreatePost;
+
+public record CreatePostCommand(string AuthorId,CreatePostDto Dto) : IRequest<Result<PostDto>>;
---
+++ Social-Media-Chatting-APP-Service/Features/Posts/Commands/CreatePost/CreatePostCommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using MediatR;
+using Social_Media_Chatting_APP_Domain.Entities;
+using Social_Media_Chatting_APP_Domain.Entities.Enums;
+using Social_Media_Chatting_APP_Domain.Interfaces;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.PostsDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Posts.Commands.CreatePost;
+
+public class CreatePostCommandHandler(
+    IUnitOfWork unitOfWork,
+    IMapper mapper
+) : IRequestHandler<CreatePostCommand, Result<PostDto>>
+{
+    public async Task<Result<PostDto>> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+    {
+        var postRepo = unitOfWork.GetRepository<Post, Guid>();
+
+
+        var post = new Post()
+        {
+            AuthorId = request.AuthorId,
+            Content = request.Dto.Content,
+            QuoteContent = request.Dto.QuoteContent,
+            PostType = request.Dto.PostType,
+            CreatedAt = DateTime.UtcNow,
+        };
+
+        if (request.Dto.PostType == PostType.Repost || request.Dto.PostType == PostType.Quote)
+        {
+            var originalPost = await postRepo.FindAsync(p => p.Id == request.Dto.OriginalPostId);
+            if (originalPost is null || originalPost.IsDeleted == true)
+            {
+                return Error.NotFound("Post.NotFound", "Original post not found");
+            }
+        }
+
+        await postRepo.AddAsync(post);
+        await unitOfWork.SaveChangesAsync();
+
+        if (request.Dto.MediaAsset is not null && request.Dto.MediaAsset.Any())
+        {
+            var mediaAssetRepo = unitOfWork.GetRepository<MediaAsset, Guid>();
+            foreach (var asset in request.Dto.MediaAsset)
+            {
+                var mediaAsset = await mediaAssetRepo.FindAsync(m => m.PublicId == asset.PublicId && !m.IsDeleted);
+                if (mediaAsset is not null)
+                {
+                    mediaAsset.PostId = post.Id;
+                }
+            }
+        }
+        await unitOfWork.SaveChangesAsync();
+        var mappedPost = mapper.Map<PostDto>(post);
+        return Result<PostDto>.Ok(mappedPost);
+    }
+}
