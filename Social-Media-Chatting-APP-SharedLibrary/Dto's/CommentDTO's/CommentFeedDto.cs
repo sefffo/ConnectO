170cf8c3dab2695612ed5b1c111bfe0d7746bd1b
pc <pc@DESKTOP-2N68CP9>
2026-08-13 20:32:03 +0300

    fix(CommentsDto) : change the CommentFeedDto.cs location in the CommentDto folder

---
+++ Social-Media-Chatting-APP-SharedLibrary/Dto's/CommentDTO's/CommentFeedDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Social_Media_Chatting_APP_SharedLibrary.Dto_s.CommentDTO_s;
+
+public record CommentFeedDto(
+    List<CommentDto> Comments,
+    DateTime? NextCursor,
+    bool HasNextPage
+);
--- Social-Media-Chatting-APP-SharedLibrary/Dto's/CommonDTO's/CommentFeedDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Social_Media_Chatting_APP_SharedLibrary.Dto_s.CommentDTO_s;
-
-namespace Social_Media_Chatting_APP_SharedLibrary.Dto_s.commonDTO_s;
-
-public record CommentFeedDto(
-    List<CommentDto> Comments,
-    DateTime? NextCursor,
-    bool HasNextPage
-);
