0cdd6ebf826a3cde721a8d5aa0183a7f35a79b92
pc <pc@DESKTOP-2N68CP9>
2026-08-14 14:47:57 +0300

    Feat(CommentReplies) : add the comment replies Query

---
+++ Social-Media-Chatting-APP-Service/Features/Comments/Queries/GetCommentReplies/GetCommentRepliesQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.CommentDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Service.Features.Comments.Queries.GetCommentReplies;
+
+public record GetCommentRepliesQuery(  
+    Guid PostId,
+    Guid UserId // for is liked by ME 
+    ,
+    DateTime? Cursor,
+    int Limit = 20) : IRequest<Result<CommentFeedDto>>;
