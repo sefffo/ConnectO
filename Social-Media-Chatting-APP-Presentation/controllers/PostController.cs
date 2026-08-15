1cba09fe632a8bd90438f216abf9ddbb2265364f
pc <pc@DESKTOP-2N68CP9>
2026-08-15 21:53:52 +0300

    Fear(Controller) : adding the PostController.cs and edited the SoftDeletePostCommand.cs

---
+++ Social-Media-Chatting-APP-Presentation/controllers/PostController.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Claims;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Social_Media_Chatting_APP_Service.Features.Posts.Commands.CreatePost;
+using Social_Media_Chatting_APP_Service.Features.Posts.Commands.SoftDeletePost;
+using Social_Media_Chatting_APP_Service.Features.Posts.Queries.GetPostById;
+using Social_Media_Chatting_APP_Service.Features.Posts.Queries.GetUSerPosts;
+using Social_Media_Chatting_APP_SharedLibrary.Dto_s.PostsDTO_s;
+using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
+
+namespace Social_Media_Chatting_APP_Presentation.Controllers;
+
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+public class PostController(ISender sender) : ApiBaseController
+{
+    [HttpPost]
+    public async Task<ActionResult<Result<PostDto>>> Post(CreatePostDto createPostDto)
+    {
+        // find the user and check first 
+        var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await sender.Send(new CreatePostCommand(
+            user,
+            createPostDto
+        ));
+        return HandleResult(result);
+    }
+
+    [HttpDelete("{postId}")]
+    public async Task<ActionResult<Result<PostDto>>> Post([FromQuery]Guid postId)
+    {
+        var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await sender.Send(new SoftDeletePostCommand(
+            user,
+            postId
+        ));
+        return HandleResult(result);
+    }
+
+    [HttpGet("{postId}")]
+    public async Task<ActionResult<Result<PostDto>>> GetPostById([FromQuery] Guid postId)
+    {
+        var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await sender.Send(new GetPostByIdQuery(postId , user));
+        return HandleResult(result);
+    }
+
+    [HttpGet("posts/user/{authorId}")]
+    public async Task<ActionResult<Result<PostFeedDto>>> GetUserPosts([FromRoute] string authorId , [FromQuery] DateTime? cursor , [FromQuery] int limit = 20)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await sender.Send(new GetUserPostsQuery(authorId ,userId,cursor , limit ));
+        return HandleResult(result);
+    }
+    
+    
+    
+}
--- Social-Media-Chatting-APP-Service/Features/Posts/Commands/SoftDeletePost/SoftDeletePostCommand.cs
+++ Social-Media-Chatting-APP-Service/Features/Posts/Commands/SoftDeletePost/SoftDeletePostCommand.cs
@@ -4,6 +4,6 @@ using Social_Media_Chatting_APP_SharedLibrary.SharedResponse;
 namespace Social_Media_Chatting_APP_Service.Features.Posts.Commands.SoftDeletePost;
 
 public record SoftDeletePostCommand(
-    Guid AuthorId,
+    string AuthorId,
     Guid PostId
     ) : IRequest<Result<string>>;
