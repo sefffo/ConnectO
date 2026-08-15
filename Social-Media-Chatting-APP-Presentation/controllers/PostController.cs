57b63a03322edba0fa12e9050eff12db97dab3b7
pc <pc@DESKTOP-2N68CP9>
2026-08-15 22:49:50 +0300

    Feat( Repost endpoint ) : adding Create repost end point in PostController.cs

--- Social-Media-Chatting-APP-Presentation/controllers/PostController.cs
+++ Social-Media-Chatting-APP-Presentation/controllers/PostController.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Social_Media_Chatting_APP_Service.Features.Posts.Commands.CreatePost;
+using Social_Media_Chatting_APP_Service.Features.Posts.Commands.CreateRepost;
 using Social_Media_Chatting_APP_Service.Features.Posts.Commands.SoftDeletePost;
 using Social_Media_Chatting_APP_Service.Features.Posts.Queries.GetPostById;
 using Social_Media_Chatting_APP_Service.Features.Posts.Queries.GetUSerPosts;
@@ -30,7 +31,7 @@ public class PostController(ISender sender) : ApiBaseController
 
     [HttpDelete("{postId}")]
     [Authorize]
-    public async Task<ActionResult<Result<PostDto>>> Post([FromRoute]Guid postId)
+    public async Task<ActionResult<Result<PostDto>>> Post([FromRoute] Guid postId)
     {
         var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
         var result = await sender.Send(new SoftDeletePostCommand(
@@ -45,19 +46,26 @@ public class PostController(ISender sender) : ApiBaseController
     public async Task<ActionResult<Result<PostDto>>> GetPostById([FromRoute] Guid postId)
     {
         var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        var result = await sender.Send(new GetPostByIdQuery(postId , user));
+        var result = await sender.Send(new GetPostByIdQuery(postId, user));
         return HandleResult(result);
     }
 
     [Authorize]
     [HttpGet("posts/user/{authorId}")]
-    public async Task<ActionResult<Result<PostFeedDto>>> GetUserPosts([FromRoute] string authorId , [FromQuery] DateTime? cursor , [FromQuery] int limit = 20)
+    public async Task<ActionResult<Result<PostFeedDto>>> GetUserPosts([FromRoute] string authorId,
+        [FromQuery] DateTime? cursor, [FromQuery] int limit = 20)
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        var result = await sender.Send(new GetUserPostsQuery(authorId ,userId,cursor , limit ));
+        var result = await sender.Send(new GetUserPostsQuery(authorId, userId, cursor, limit));
         return HandleResult(result);
     }
-    
-    
-    
+
+    // create Repost 
+    [HttpPost("repost")]
+    public async Task<ActionResult<Result<PostDto>>> CreateRepost([FromBody] CreateRepostDto createRepostDto)
+    {
+        var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var result = await sender.Send(new CreateRepostCommand(user, createRepostDto));
+        return HandleResult(result);
+    }
 }
