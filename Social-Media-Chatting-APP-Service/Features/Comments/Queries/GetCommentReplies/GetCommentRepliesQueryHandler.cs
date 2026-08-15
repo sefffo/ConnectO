dc3ce55254b9545379c268da10126ebc2e78cfd6
pc <pc@DESKTOP-2N68CP9>
2026-08-15 21:12:27 +0300

    Fix(Comment Replies) : fixing the replies Logic

--- Social-Media-Chatting-APP-Service/Features/Comments/Queries/GetCommentReplies/GetCommentRepliesQueryHandler.cs
+++ Social-Media-Chatting-APP-Service/Features/Comments/Queries/GetCommentReplies/GetCommentRepliesQueryHandler.cs
@@ -17,29 +17,28 @@ public class GetCommentRepliesQueryHandler(
         CancellationToken cancellationToken)
     {
         var commentRepo = unitofwork.GetRepository<Comment,Guid>();
-        var spec = new CommentRepliesSpecification(request.CommentId, request.Cursor , request.Limit);
+        var spec = new CommentRepliesSpecification(request.CommentId, request.Cursor, request.Limit);
         
         // check el parent comment asln mawgood 
-        var parentComment = await commentRepo.FindAllAsync(spec);
-        if (parentComment == null)
-        {
+        var parentComment = await commentRepo.GetByIdAsync(request.CommentId);
+        if (parentComment == null || parentComment.IsDeleted)
             return Error.NotFound("Parent-Comment.NotFound", "Comment not found");
-        }
+        var replies = await commentRepo.FindAllAsync(spec);
 
         // el pagination b'a 
         
-        var hasNextPage = parentComment.Count()>request.Limit;
+        var hasNextPage = replies.Count()>request.Limit;
         if (hasNextPage)
         {
             //yrg3 el limit
-            parentComment =  parentComment.Take(request.Limit).ToList();
+            replies =  replies.Take(request.Limit).ToList();
         }
         // lw 3ndha next page yb'a akher comment hwa el next Cursor lw la' yb'a DateTime(null)
-        var nextCursor = hasNextPage ? parentComment.Last().CreatedAt : (DateTime?)null;
+        var nextCursor = hasNextPage ? replies.Last().CreatedAt : (DateTime?)null;
         
-        var mappedComments = mapper.Map<List<CommentDto>>(parentComment);
+        var mappedComments = mapper.Map<List<CommentDto>>(replies);
         
-        foreach (var (commentDto, comment) in mappedComments.Zip(parentComment))
+        foreach (var (commentDto, comment) in mappedComments.Zip(replies))
         {
             commentDto.IsLikedByMe = comment.CommentLikes.Any(l => l.UserId == request.UserId.ToString());
             commentDto.LikeCount = comment.CommentLikes.Count;
